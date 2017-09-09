995debd5bb6918b9cc15e016ec88fcbca52b0a78
Frank <xuzy213@gmail.com>
2017-09-09 23:11:08 +1200

    modify the rules description

--- Pig-Game 3.0/app.js
+++ Pig-Game 3.0/app.js
@@ -1,14 +1,33 @@
 /*
-GAME RULES 2.0:
+
+### Pig Game
+GAME RULES:
 
 - The game has 2 players, playing in rounds
 - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-- The player will lose current score if rolls two same number.
+- The first player to reach 100 points  by default on GLOBAL score wins the game.
 
+
+
+
+### Pig Game 2.0
 Add an input field to the HTML where players can set the winning score.
+Add GAME RULES:
+
+- The player will lose current score if rolls two same number.
+- The first player to reach 100 points  by default on GLOBAL score wins the game.
+
+
+
+
+### Pig Game 3.0
+Change GAME RULES:
+
+- In each turn, a player rolls two dices as many times as he whishes. Each result get added to his ROUND score
+
+
 
 */
 // dice = Math.floor(Math.random() * 6) + 1;
