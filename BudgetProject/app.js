6fee177ef55ce2dddd22c0aa94b4b6385c00a2e6
Frank <xuzy213@gmail.com>
2017-09-14 21:03:45 +1200

    add "addListItem" to add new input inc or exp into the page

--- BudgetProject/app.js
+++ BudgetProject/app.js
@@ -69,7 +69,9 @@ var UIController = (function() {
 		inputType: '.add__type',
 		inputDescription: '.add__description',
 		inputValue: '.add__value',
-		inputButton: '.add__btn'
+		inputButton: '.add__btn',
+		incomeContainer: '.income__list',
+		expensesContainer: '.expenses__list'
 	}
 
 	return {
@@ -79,6 +81,28 @@ var UIController = (function() {
 				description: document.querySelector(DOMString.inputDescription).value,
 				value: document.querySelector(DOMString.inputValue).value
 				};  
+		},
+
+		addListItem: function(obj, type) {
+			var html, newHtml, element;
+			// Create HTML string with placeholder text
+			if (type === 'inc') {
+				element = DOMString.incomeContainer;
+				html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+			} else if (type === 'exp') {
+				element = DOMString.expensesContainer;
+				html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+			}
+			
+			// Replace the placeholder text with some actual data			
+			newHtml = html.replace('%id%', obj.id);
+			// rewrite newHtml with html
+			newHtml = newHtml.replace('%description%', obj.description);
+			newHtml = newHtml.replace('%value%',  obj.value);
+
+			// Insert the HTML into the DOM
+			document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+
 		},
 
 		getDOMstrings: function(){
@@ -121,7 +145,7 @@ var controller = (function(budgetCtrl, UICtrl){
 		// 2. Add the item to the budget controller
 		newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 		// 3. Add the item to the UT
-
+		UICtrl.addListItem(newItem, input.type);
 		// 4. Calculate the budget
 
 		// 5. Display the budget on the UI
